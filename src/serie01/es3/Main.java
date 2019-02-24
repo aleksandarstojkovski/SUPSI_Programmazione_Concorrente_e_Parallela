76054c9cc2d412a6f6be09f864aac6c0887acc55
aleksandarstojkovski <NadaRisto2>
2019-02-24 14:55:56 +0100

    s1 es 3 finito

---
+++ src/serie01/es3/Main.java
@@ -0,0 +1,40 @@
+package serie01.es3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[100000];
+        int numberOfThreads=10;
+        int firstElementOfIntervall=0;
+        int secondElementOfIntervall=9999;
+        List<Thread> listOfThreads = new ArrayList<>();
+
+        System.out.println("Riempio l'array di " + array.length + " elementi random");
+        for(int i=0;i<array.length;i++){
+            array[i]=(int) (Math.random() * (100 - 1)) + 1;
+            // debbug
+            // array[i]=1;
+        }
+
+        System.out.println("Creo " + numberOfThreads + " threads");
+
+        for (int i=0;i<10;i++){
+            MyThread mt = new MyThread(array,firstElementOfIntervall,secondElementOfIntervall);
+            Thread t = new Thread(mt);
+            listOfThreads.add(t);
+            firstElementOfIntervall+=1000;
+            secondElementOfIntervall+=1000;
+        }
+
+        System.out.println("Starto i " + numberOfThreads + " threads");
+
+        for (Thread t : listOfThreads){
+            t.start();
+        }
+
+    }
+
+}
---
+++ src/serie01/es3/MyThread.java
@@ -0,0 +1,28 @@
+package serie01.es3;
+
+public class MyThread implements Runnable {
+
+    private int[] arrayOfIntegers;
+    private int firstElementOfIntervall;
+    private int lastElementOfIntervall;
+
+    MyThread(int[] arrayOfIntegers, int firstElementOfIntervall, int lastElementOfIntervall){
+        this.arrayOfIntegers=arrayOfIntegers;
+        this.firstElementOfIntervall=firstElementOfIntervall;
+        this.lastElementOfIntervall=lastElementOfIntervall;
+    }
+
+    @Override
+    public void run() {
+        int sum=0;
+        // ho reso appositamente il calcolo piu difficile, facendoglielo fare 10000000 di volte
+        // in questo modo posso osservare i threads che lavorano
+        for (int j=0;j<10000000;j++) {
+            sum=0;
+            for (int i = firstElementOfIntervall; i < lastElementOfIntervall; i++) {
+                sum += arrayOfIntegers[i];
+            }
+        }
+        System.out.println("Somma degli elementi nell'intervallo [" + firstElementOfIntervall + ";" + lastElementOfIntervall + "]" + " = " + sum);
+    }
+}
