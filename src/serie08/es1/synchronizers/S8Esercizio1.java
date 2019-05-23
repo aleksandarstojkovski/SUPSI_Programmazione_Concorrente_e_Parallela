9c75148abe5197bb0cc5050ec101a87d850df72f
aleksandarstojkovski <NadaRisto2>
2019-05-23 11:41:21 +0200

    es1 finito

--- src/serie08/es1/synchronizers/S8Esercizio1.java
+++ src/serie08/es1/synchronizers/S8Esercizio1.java
@@ -1,11 +1,47 @@
 package serie08.es1.synchronizers;
 
 import java.util.Random;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Calculator implements Runnable{
+
+	private int id;
+
+	Calculator(int id){
+		this.id=id;
+		// per forza nel custruttore !!!!!!!
+		S8Esercizio1.phaser.register();
+	}
+
+	@Override
+	public void run() {
+
+		S8Esercizio1.rowSum[id]=S8Esercizio1.sumRow(id);
+
+		S8Esercizio1.phaser.arriveAndAwaitAdvance();
+
+		S8Esercizio1.colSum[id]=S8Esercizio1.sumColumn(id);
+
+		S8Esercizio1.phaser.arriveAndDeregister();
+		// anche phaser.arrive() avrebbe funzionato, ma dato che non
+		// usiamo piu il phaser ci deregistriamo
+
+	}
+
+}
 
 public class S8Esercizio1 {
+
 	final static int[][] matrix = new int[10][10];
 	final static int[] rowSum = new int[matrix.length];
 	final static int[] colSum = new int[matrix[0].length];
+	// Phaser(1) è come se facessimo Phaser(0) e poi phaser.register().
+	// in pratica registriamo il main
+	final static Phaser phaser = new Phaser(1);
 
 	public static void main(String[] args) {
 		// Inizializza matrice con valori random
@@ -15,17 +51,19 @@ public class S8Esercizio1 {
 		System.out.println("Matrice:");
 		printMatrix();
 
-		// Calcola somma delle righe
-		for (int row = 0; row < matrix.length; row++)
-			rowSum[row] = sumRow(row);
+		for (int i=0;i<matrix.length;i++){
+			Calculator c = new Calculator(i);
+			Thread t = new Thread(c);
+			t.start();
+		}
+
+		phaser.arriveAndAwaitAdvance();
 
 		// Stampa somma delle righe
 		System.out.println("Somme delle righe:");
 		printArray(rowSum);
 
-		// Calcola somma delle colonne
-		for (int col = 0; col < matrix[0].length; col++)
-			colSum[col] = sumColumn(col);
+		phaser.arriveAndAwaitAdvance();
 
 		// Stampa somma delle colonne
 		System.out.println("Somme delle colonne:");
